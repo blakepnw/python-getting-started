7068c985190a635c886a28a66c65f73690d5b9cf
Blake Markham <bmarkham@bmarkha-ltmbn36.internal.salesforce.com>
2022-01-23 22:16:55 -0800

    Demo changes

---
+++ hello/plotter.py
@@ -0,0 +1,7 @@
+import numpy as np # we will use this later, so import it now
+
+from bokeh.io import output_notebook, show
+from bokeh.plotting import figure
+
+def index(request):
+    output_notebook()
